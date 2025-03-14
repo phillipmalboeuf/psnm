dbfe7fca0ce836a32cebe0196108668b8c06439f
Phillip Malboeuf <phil@phils.computer>
2025-03-14 10:48:39 -0400

    hr fix

--- src/lib/components/Text.svelte
+++ src/lib/components/Text.svelte
@@ -47,7 +47,7 @@
 {#key item.sys.id}
 <section class="flex flex--gapped {item.fields.couleur || ''}" class:no-media={!media} class:padded={!!item.fields.couleur} class:full={item.fields.full} class:half={item.fields.full && (item.fields.id?.includes('une-question') || item.fields.id?.includes('ce-profil-vous-interesse'))} class:vertical={item.fields.vertical} class:alt={item.fields.alt} class:inverse={item.fields.inverse} class:center={item.fields.center} id={item.fields.id}>
   {#if !item.fields.couleur}
-  {#if item.fields.titre}
+  {#if item.fields.titre || !item.fields.center}
   <hr />
   {:else}
   <div />
