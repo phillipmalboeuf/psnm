5c2b1eb695642b0d3f2bf2a39526887b75dd6952
Phillip Malboeuf <phil@phils.computer>
2025-08-12 16:55:03 -0400

    cache control

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -5,6 +5,7 @@ export const handle: Handle = async ({ event, resolve }) => {
 	const response = await resolve(event);
 
   response.headers.set('Vercel-CDN-Cache-Control', 'max-age=3600000');
+  response.headers.set('Cache-Control', 'max-age=3600000');
 
 	return response;
 };
