bd5d01bd5e934266ea7e6568c490cf18cef7d6fe
Phillip Malboeuf <phil@phils.computer>
2025-08-14 10:19:35 -0400

    deploy

--- src/routes/api/clear/+server.ts
+++ src/routes/api/clear/+server.ts
@@ -4,5 +4,13 @@ import { redis } from '$lib/clients/redis'
 
 export const POST: RequestHandler = async () => {
   await redis.flushAll()
+
+  await fetch('https://api.vercel.com/v1/integrations/deploy/prj_C770TogCQomhk6bSLnYIDRR0Ll90/i9YHcnBBAi', {
+    method: 'POST',
+    headers: {
+      // 'Authorization': `Bearer ${process.env.VERCEL_TOKEN}`
+    }
+  })
+  
   return json({ success: true })
 }
