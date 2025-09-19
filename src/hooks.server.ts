c60ab6d372961e7a7b4df3d1d84e4f34a9a8716e
Phillip Malboeuf <phil@phils.computer>
2025-09-19 12:45:52 -0400

    etag

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -6,6 +6,7 @@ export const handle: Handle = async ({ event, resolve }) => {
 
   response.headers.set('Vercel-CDN-Cache-Control', 'max-age=3600000');
   response.headers.set('Cache-Control', 'max-age=3600000');
+  response.headers.set('Etag', `"${process.env.VERCEL_URL || 'dev'}"`);
 
 	return response;
 };
